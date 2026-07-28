dcedb1b3bd5efe8c0bfaf42ebefaf023318997f2
leonpeon <59907683+leonpeon@users.noreply.github.com>
2026-07-28 12:42:15 +1000

    Make thumbnail generator for all images in images file

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ from PIL import Image
 import tkinter as tk
 import os
 
+
+# Creates thumbnails for all images in the images file.
 for image in os.listdir("images"):
-    print(image)
+    end_image = os.path.splitext(image)[0] + ".thumbnail"
+    if image != end_image:
+        try:
+            with Image.open(image) as im:
+                im.thumbnail((50,50))
+                im.save(end_image, "JPEG")
+        except OSError:
+            print("Cannot create thumbnail for ", image)
+        print(image)
 
 window = tk.Tk()
 
@@ -17,6 +27,9 @@ x = (screen_width - window_width) // 2
 y = (screen_height - window_height) // 2 - 30
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+canvas = tk.Canvas(window)
+# scrollbar = tk.Scrollbar(window, orient="vertical", command=canvas.yview)
 
 window.mainloop()
 
