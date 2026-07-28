385c03e2aeb96ef3354ade781ad2b334aa6a5836
leonpeon <59907683+leonpeon@users.noreply.github.com>
2026-07-28 13:06:24 +1000

    Update thumbnail generator to put thumbnails into a seperate folder

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ import os
 
 # Creates thumbnails for all images in the images file.
 for image in os.listdir("images"):
-    end_image = os.path.splitext(image)[0] + ".thumbnail"
-    if image != end_image:
-        try:
-            with Image.open(image) as im:
-                im.thumbnail((50,50))
-                im.save(end_image, "JPEG")
-        except OSError:
-            print("Cannot create thumbnail for ", image)
-        print(image)
+    image_path = os.path.join("images", image)
+    file_name = os.path.splitext(image)[0]
+    end_folder = os.path.join("thumbnails", file_name + "_thumbnail.jpg")
+
+    try:
+        with Image.open(image_path) as im:
+            im.thumbnail((50,50))
+            im.save(end_folder, "JPEG")
+    except OSError:
+        print("Cannot create thumbnail for", image)
+    print(image)
 
 window = tk.Tk()
 
