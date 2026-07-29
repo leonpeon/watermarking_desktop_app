77eba5278e0491084431b14d00ed0271c1a73a20
leonpeon <59907683+leonpeon@users.noreply.github.com>
2026-07-29 16:55:32 +1000

    Stop the user from scrolling past the scrollable area

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from tkinter import simpledialog
 import os
 
+# Shows the image on the left screen if clicked on.
 def image_select(image_name):
     if image_name == "start":
         file_name = os.path.join("images", os.listdir("images")[0])
@@ -20,7 +21,6 @@ def image_select(image_name):
     side_image_file = ImageTk.PhotoImage(image.resize((new_width, new_height), Image.Resampling.LANCZOS))
     side_image.image = side_image_file
     side_image.file_path = file_name
-    print(file_name)
     side_image.configure(image=side_image_file)
     side_image.pack(padx=50)
 
@@ -39,17 +39,20 @@ def watermark(img, watermark_text):
               stroke_fill="grey"
               )
 
+    # Saves image to "watermarked_images" folder
     file = os.path.splitext(os.path.basename(img))[0]
     end_folder = os.path.join("watermarked_images", file + "_watermark.jpg")
     image.convert("RGB").save(end_folder, "JPEG")
     show_final_image(end_folder)
 
+# Asks user for what their watermark should be
 def customise_watermark():
     user_text = simpledialog.askstring("Watermark", "Enter your watermark text: ")
     image = side_image.file_path
     print(image)
     watermark(image, user_text)
 
+# Displays processed image to the user
 def show_final_image(image):
     final_img_window = tk.Toplevel(window)
     final_img_window.title("Watermarked Image")
@@ -63,7 +66,6 @@ def show_final_image(image):
     label = tk.Label(final_img_window, image=photo)
     label.image = photo
     label.pack()
-
 
 # Creates thumbnails for all images in the images file.
 for image in os.listdir("images"):
@@ -81,6 +83,7 @@ for image in os.listdir("images"):
             print("Cannot create thumbnail for", image)
         print(image)
 
+# Creating the window
 window = tk.Tk()
 window.title("Watermark App")
 
@@ -93,7 +96,7 @@ y = (screen_height - window_height) // 2 - 30
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-#FRAMES
+# Creating the frames
 left_frame = tk.LabelFrame(window, text="Image Preview", height=700, width=240, padx=20, pady=5)
 left_frame.grid(column=0, row=0, rowspan=2, padx=20)
 left_frame.grid_propagate(False)
@@ -105,6 +108,7 @@ right_frame.grid_propagate(False)
 button_frame = tk.Frame(window)
 button_frame.grid(column=1, row=2)
 
+# "Select Image" button
 button = tk.Button(button_frame, text="Select Image", width=15, command=customise_watermark)
 button.pack(pady=20, ipadx=40)
 
@@ -115,11 +119,21 @@ image_select("start")
 
 # RIGHT FRAME
 canvas = tk.Canvas(right_frame, background="pink", height=400, width=400)
-canvas.configure(scrollregion=canvas.bbox('all'))
 canvas.pack()
 
 # mouse wheel scrolling
-canvas.bind("<MouseWheel>", lambda event: canvas.yview_scroll(-int(event.delta/60), "units"))
+def mousewheel(event):
+    top, bottom = canvas.yview()
+    delta = -int(event.delta/60)
+
+    if delta < 0 and top <= 0:
+        return
+    if delta > 0 and bottom >= 1:
+        return
+
+    canvas.yview_scroll(delta, "units")
+
+canvas.bind("<MouseWheel>", mousewheel)
 
 # scrollbar
 scrollbar = tk.Scrollbar(right_frame, relief="raised", command=canvas.yview)
@@ -128,13 +142,17 @@ scrollbar.place(relx=1, rely=0, relheight=1, anchor="ne")
 
 # Frame inside canvas
 image_frame = tk.Frame(canvas, background="pink")
-image_frame.configure(width=400)
+image_frame.bind(
+    "<Configure>",
+    lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
+)
 canvas.create_window(
     (0, 0),
     window=image_frame,
     anchor="nw"
 )
 
+# Loops through images and turns them into buttons
 label_list = []
 for thumbnail in os.listdir("thumbnails"):
     file = os.path.join("thumbnails", thumbnail)
@@ -145,6 +163,7 @@ for thumbnail in os.listdir("thumbnails"):
     label.image = img_obj
     label_list.append(label)
 
+# Sorts the images into rows and columns of three
 column = 0
 row = 0
 for img in label_list:
